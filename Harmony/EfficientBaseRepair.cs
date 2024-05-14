4badf942e5dfa96807dfc16366ad450a6a5db52a
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-05-14 19:02:28 +0200

    wip setting up

---
+++ Harmony/EfficientBaseRepair.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+namespace Harmony
+{
+    public class EfficientBaseRepair : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            var harmony = new HarmonyLib.Harmony(_modInstance.Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Harmony/MyBlock.cs
@@ -0,0 +1,28 @@
+﻿
+// <property name="Class" value="MyBlock, SampleProject" />
+class MyBlock : Block
+{
+    public override void Init()
+    {
+        Log.Out($"Hello from {GetType()}!");
+        base.Init();
+
+        // If you want the OnBlockLoaded / OnBlockUnloaded to fire, set this to true
+        IsNotifyOnLoadUnload = true;
+    }
+
+    // Only fires if IsNotifyOnLoadUnload is set to true
+    public override void OnBlockLoaded(WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue)
+    {
+
+        Log.Out("OnBlockLoaded() ");
+        base.OnBlockLoaded(_world, _clrIdx, _blockPos, _blockValue);
+    }
+
+    // Only fires if IsNotifyOnLoadUnload is set to true
+    public override void OnBlockUnloaded(WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue)
+    {
+        Log.Out("OnBlockUnloaded()");
+        base.OnBlockUnloaded(_world, _clrIdx, _blockPos, _blockValue);
+    }
+}
