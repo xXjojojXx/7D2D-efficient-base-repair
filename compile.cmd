5a0c2119ac7db6df047551e8a7faecb7637c0445
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-05-16 18:09:16 +0200

    setup compiling script

---
+++ compile.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+dotnet build --no-incremental .\EfficientBaseRepair.csproj
+
+if exist "EfficientBaseRepair.zip" DEL "EfficientBaseRepair.zip"
+
+7z.exe a "EfficientBaseRepair.zip" ^
+    config ^
+    UIAtlases ^
+    EfficientBaseRepair.dll ^
+    ModInfo.xml > nul
+
+DEL .\EfficientBaseRepair.dll
+DEL .\EfficientBaseRepair.pdb
+
+set MOD_PATH="%PATH_7D2D%\Mods\EfficientBaseRepair"
+
+if exist %MOD_PATH% RMDIR /s /q %MOD_PATH%
+
+mkdir %MOD_PATH%
+
+cd %MOD_PATH%
+
+7z.exe x "%~dp0/EfficientBaseRepair.zip" > nul
