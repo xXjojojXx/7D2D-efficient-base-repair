122e65c0af73b4fcfb24823334ca89b7f52176cc
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-05-18 16:42:29 +0200

    re-implement basic commands

--- Harmony/BlockEfficientBaseRepair.cs
+++ Harmony/BlockEfficientBaseRepair.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using Audio;
+using Platform;
+using UnityEngine;
 
 class BlockEfficientBaseRepair : BlockSecureLoot
 {
@@ -8,13 +10,10 @@ class BlockEfficientBaseRepair : BlockSecureLoot
     private const string TURN_ON_CMD = "turn_repair_on";
     private const string TURN_OFF_CMD = "turn_repair_off";
 
+    private int TakeDelay;
+
     // copied from ocbClaimAutoRepair and adapted from BlockLandClaim
-    public override void OnBlockEntityTransformAfterActivated(
-        WorldBase _world,
-        Vector3i _blockPos,
-        int _cIdx,
-        BlockValue _blockValue,
-        BlockEntityData _ebcd)
+    public override void OnBlockEntityTransformAfterActivated(WorldBase _world, Vector3i _blockPos, int _cIdx, BlockValue _blockValue, BlockEntityData _ebcd)
     {
         if (_ebcd == null)
             return;
@@ -40,13 +39,8 @@ class BlockEfficientBaseRepair : BlockSecureLoot
         base.OnBlockEntityTransformAfterActivated(_world, _blockPos, _cIdx, _blockValue, _ebcd);
     }
 
-
     // copied from ocbClaimAutoRepair
-    public override void OnBlockAdded(
-        WorldBase _world,
-        Chunk _chunk,
-        Vector3i _blockPos,
-        BlockValue _blockValue)
+    public override void OnBlockAdded(WorldBase _world, Chunk _chunk, Vector3i _blockPos, BlockValue _blockValue)
     {
         if (_blockValue.ischild || _world.GetTileEntity(_chunk.ClrIdx, _blockPos) is TileEntityEfficientBaseRepair)
             return;
@@ -61,194 +55,163 @@ class BlockEfficientBaseRepair : BlockSecureLoot
         tileEntity.SetContainerSize(LootSize);
         _chunk.AddTileEntity(tileEntity);
 
+        TakeDelay = this.Properties.GetInt("TakeDelay");
+
         base.OnBlockAdded(_world, _chunk, _blockPos, _blockValue);
     }
 
-    // copied from ocbClaimAutoRepair
-	public override BlockActivationCommand[] GetBlockActivationCommands(
-		WorldBase _world,
-		BlockValue _blockValue,
-		int _clrIdx,
-		Vector3i _blockPos,
-		EntityAlive _entityFocusing)
-	{
-		TileEntityEfficientBaseRepair tileEntity = _world.GetTileEntity(_clrIdx, _blockPos) as TileEntityEfficientBaseRepair;
-		BlockActivationCommand[] commands = base.GetBlockActivationCommands(_world, _blockValue, _clrIdx, _blockPos, _entityFocusing);
-
-		Array.Resize(ref commands, commands.Length + 2);
-		commands[commands.Length - 2] = new BlockActivationCommand("take", "hand", false);
-
-		string cmd = tileEntity.is_activated ? TURN_OFF_CMD : TURN_ON_CMD;
-		commands[commands.Length - 1] = new BlockActivationCommand(cmd, "electric_switch", true);
-
-		// if (this.CanPickup)
-		// 	commands[commands.Length - 2].enabled = true;
-
-		// else if ((double)EffectManager.GetValue(PassiveEffects.BlockPickup, _entity: _entityFocusing, tags: _blockValue.Block.Tags) > 0.0)
-		// 	commands[commands.Length - 2].enabled = true;
-
-		// else
-		// 	commands[commands.Length - 2].enabled = false;
-
-		return commands;
-	}
-
-    // copied from ocbClaimAutoRepair
-    public override bool OnBlockActivated(
-        string _commandName,
-        WorldBase _world,
-        int _cIdx,
-        Vector3i _blockPos,
-        BlockValue _blockValue,
-        EntityAlive _player)
+    public override BlockActivationCommand[] GetBlockActivationCommands(WorldBase _world, BlockValue _blockValue, int _clrIdx, Vector3i _blockPos, EntityAlive _entityFocusing)
     {
-        if (!(_world.GetTileEntity(_cIdx, _blockPos) is TileEntityEfficientBaseRepair tileEntity))
-            return false;
+        TileEntityEfficientBaseRepair tileEntity = _world.GetTileEntity(_clrIdx, _blockPos) as TileEntityEfficientBaseRepair;
 
-        if (_commandName == "take")
+        Log.Out(tileEntity == null ? "Null TileEntityEfficientBaseRepair": "Not null TileEntityEfficientBaseRepair");
+
+        string cmd_activate = tileEntity.is_activated ? TURN_OFF_CMD : TURN_ON_CMD;
+        bool is_locked = tileEntity.IsLocked();
+
+        Log.Out("cmd_activate: " + cmd_activate);
+
+        return new BlockActivationCommand[6]
         {
-            // Copied from vanilla Block::OnBlockActivated
-            bool flag = this.CanPickup;
-            if ((double)EffectManager.GetValue(PassiveEffects.BlockPickup, _entity: _player, tags: _blockValue.Block.Tags) > 0.0)
-                flag = true;
+            new BlockActivationCommand("Search", "search", true),
+            new BlockActivationCommand("lock", "lock", !is_locked),
+            new BlockActivationCommand("keypad", "keypad", true),
+            new BlockActivationCommand("unlock", "unlock", is_locked),
+            new BlockActivationCommand(TURN_ON_CMD, "electric_switch", true),
+            new BlockActivationCommand("take", "hand", true),
+        };
 
-            if (!flag)
-                return false;
-
-            if (!_world.CanPickupBlockAt(_blockPos, _world.GetGameManager().GetPersistentLocalPlayer()))
-            {
-                _player.PlayOneShot("keystone_impact_overlay");
-                return false;
-            }
-
-            if (_blockValue.damage > 0)
-            {
-                GameManager.ShowTooltip(_player as EntityPlayerLocal, Localization.Get("ttRepairBeforePickup"), "ui_denied");
-                return false;
-            }
-
-            ItemStack itemStack = Block.list[_blockValue.type].OnBlockPickedUp(_world, _cIdx, _blockPos, _blockValue, _player.entityId);
-            if (!_player.inventory.CanTakeItem(itemStack) && !_player.bag.CanTakeItem(itemStack))
-            {
-                GameManager.ShowTooltip(_player as EntityPlayerLocal, Localization.Get("xuiInventoryFullForPickup"), "ui_denied");
-                return false;
-            }
-
-            TakeItemWithTimer(_cIdx, _blockPos, _blockValue, _player);
-            return false;
-
-        }
-        else if (_commandName == TURN_OFF_CMD || _commandName == TURN_ON_CMD)
-        {
-            tileEntity.is_activated = !tileEntity.is_activated;
-
-            if (!tileEntity.is_activated)
-                return true;
-
-            Dictionary<string, int> missing_items = tileEntity.FindAndRepairDamagedBlocks(
-                world: _player.world,
-                max_iterations: this.Properties.GetInt("MaxBfsIterations"),
-                need_materials: this.Properties.GetBool("NeedsMaterials")
-            );
-
-            Log.Out($"{missing_items.Count} missing items: ");
-
-            foreach (KeyValuePair<string, int> entry in missing_items)
-            {
-                string message = $"Missing materials: {Localization.Get(entry.Key)} x{entry.Value}";
-
-                int item_type = ItemClass.GetItem(entry.Key).type;
-
-                // TODO: find a better way to diplay missing items to the client
-                EntityPlayerLocal local_player = _player as EntityPlayerLocal;
-                local_player.AddUIHarvestingItem(itemStack: new ItemStack(new ItemValue(item_type), -entry.Value), true);
-
-                Log.Out(message);
-            }
-
-            return true;
-        }
-        else
-        {
-            return base.OnBlockActivated(_commandName, _world, _cIdx, _blockPos, _blockValue, _player);
-        }
     }
 
-    // copied from ocbClaimAutoRepair
-	public void TakeItemWithTimer(int _cIdx, Vector3i _blockPos, BlockValue _blockValue, EntityAlive _player)
+    public override string GetActivationText(WorldBase _world, BlockValue _blockValue, int _clrIdx, Vector3i _blockPos, EntityAlive _entityFocusing)
+    {
+        return Localization.Get("useWorkstation");
+    }
+
+    // copied from BlockSecureLoot
+    public override bool OnBlockActivated(string _commandName, WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue, EntityAlive _player)
+    {
+
+        if (_blockValue.ischild)
+        {
+            Vector3i parentPos = _blockValue.Block.multiBlockPos.GetParentPos(_blockPos, _blockValue);
+            BlockValue block = _world.GetBlock(parentPos);
+            return OnBlockActivated(_commandName, _world, _cIdx, parentPos, block, _player);
+        }
+
+        if (!(_world.GetTileEntity(_cIdx, _blockPos) is TileEntitySecureLootContainer tileEntitySecureLootContainer))
+        {
+            return false;
+        }
+
+        switch (_commandName)
+        {
+            case TURN_ON_CMD:
+                break;
+
+            case TURN_OFF_CMD:
+                break;
+
+            case "take":
+				TakeItemWithTimer(_cIdx, _blockPos, _blockValue, _player);
+				return true;
+
+        }
+
+        return base.OnBlockActivated(_commandName, _world, _cIdx, _blockPos, _blockValue, _player);
+    }
+
+    // copied from BlockSecureLoot
+    public override bool OnBlockActivated(WorldBase _world, int _cIdx, Vector3i _blockPos, BlockValue _blockValue, EntityAlive _player)
+    {
+        if (_blockValue.ischild)
+        {
+            Vector3i parentPos = _blockValue.Block.multiBlockPos.GetParentPos(_blockPos, _blockValue);
+            BlockValue block = _world.GetBlock(parentPos);
+            return OnBlockActivated(_world, _cIdx, parentPos, block, _player);
+        }
+
+        if (!(_world.GetTileEntity(_cIdx, _blockPos) is TileEntitySecureLootContainer tileEntitySecureLootContainer))
+        {
+            return false;
+        }
+
+        _player.AimingGun = false;
+        Vector3i blockPos = tileEntitySecureLootContainer.ToWorldPos();
+        tileEntitySecureLootContainer.bWasTouched = tileEntitySecureLootContainer.bTouched;
+        _world.GetGameManager().TELockServer(_cIdx, blockPos, tileEntitySecureLootContainer.entityId, _player.entityId);
+        return true;
+    }
+
+    // copied from BlockWorkStation
+	private void TakeItemWithTimer(int _cIdx, Vector3i _blockPos, BlockValue _blockValue, EntityAlive _player)
 	{
 		if (_blockValue.damage > 0)
 		{
-			GameManager.ShowTooltip(_player as EntityPlayerLocal, Localization.Get("ttRepairBeforePickup"), "ui_denied");
+			GameManager.ShowTooltip(_player as EntityPlayerLocal, Localization.Get("ttRepairBeforePickup"), string.Empty, "ui_denied");
+			return;
 		}
-		else
+		if (!(GameManager.Instance.World.GetTileEntity(_cIdx, _blockPos) as TileEntityEfficientBaseRepair).IsEmpty())
 		{
-			LocalPlayerUI playerUi = (_player as EntityPlayerLocal).PlayerUI;
-			playerUi.windowManager.Open("timer", true);
-			XUiC_Timer childByType = playerUi.xui.GetChildByType<XUiC_Timer>();
-			TimerEventData _eventData = new TimerEventData();
-			_eventData.Data = new object[4]
-			{
-				_cIdx,
-				_blockValue,
-				_blockPos,
-				_player
-			};
-
-            int TakeDelay = this.Properties.GetInt("TakeDelay");
-
-			_eventData.Event += new TimerEventHandler(EventData_Event);
-			childByType.SetTimer(TakeDelay, _eventData);
+			GameManager.ShowTooltip(_player as EntityPlayerLocal, Localization.Get("ttWorkstationNotEmpty"), string.Empty, "ui_denied");
+			return;
 		}
+		LocalPlayerUI playerUI = (_player as EntityPlayerLocal).PlayerUI;
+		playerUI.windowManager.Open("timer", _bModal: true);
+		XUiC_Timer childByType = playerUI.xui.GetChildByType<XUiC_Timer>();
+		TimerEventData timerEventData = new TimerEventData();
+		timerEventData.Data = new object[4] { _cIdx, _blockValue, _blockPos, _player };
+		timerEventData.Event += EventData_Event;
+		childByType.SetTimer(TakeDelay, timerEventData);
 	}
 
-    // copied from ocbClaimAutoRepair
+    // copied from BlockWorkStation
 	private void EventData_Event(TimerEventData timerData)
 	{
 		World world = GameManager.Instance.World;
-		object[] data = (object[]) timerData.Data;
-		int _clrIdx = (int) data[0];
-		BlockValue blockValue = (BlockValue) data[1];
-		Vector3i vector3i = (Vector3i) data[2];
+		object[] obj = (object[])timerData.Data;
+		int clrIdx = (int)obj[0];
+		BlockValue blockValue = (BlockValue)obj[1];
+		Vector3i vector3i = (Vector3i)obj[2];
 		BlockValue block = world.GetBlock(vector3i);
-		EntityPlayerLocal entityPlayerLocal = data[3] as EntityPlayerLocal;
+		EntityPlayerLocal entityPlayerLocal = obj[3] as EntityPlayerLocal;
 		if (block.damage > 0)
 		{
-			GameManager.ShowTooltip(entityPlayerLocal, Localization.Get("ttRepairBeforePickup"), "ui_denied");
+			GameManager.ShowTooltip(entityPlayerLocal, Localization.Get("ttRepairBeforePickup"), string.Empty, "ui_denied");
+			return;
 		}
-		else if (block.type != blockValue.type)
+		if (block.type != blockValue.type)
 		{
-			GameManager.ShowTooltip(entityPlayerLocal, Localization.Get("ttBlockMissingPickup"), "ui_denied");
+			GameManager.ShowTooltip(entityPlayerLocal, Localization.Get("ttBlockMissingPickup"), string.Empty, "ui_denied");
+			return;
 		}
-		else
+		TileEntityEfficientBaseRepair tileEntity = world.GetTileEntity(clrIdx, vector3i) as TileEntityEfficientBaseRepair;
+		if (tileEntity.IsUserAccessing())
 		{
-			TileEntityEfficientBaseRepair tileEntity = world.GetTileEntity(_clrIdx, vector3i) as TileEntityEfficientBaseRepair;
-			if (tileEntity.IsUserAccessing())
-			{
-				GameManager.ShowTooltip(entityPlayerLocal, Localization.Get("ttCantPickupInUse"), "ui_denied");
-			}
-			else
-			{
-				LocalPlayerUI uiForPlayer = LocalPlayerUI.GetUIForPlayer(entityPlayerLocal);
-				HandleTakeInternalItems(tileEntity, uiForPlayer);
-				ItemStack itemStack = new ItemStack(block.ToItemValue(), 1);
-
-				if (!uiForPlayer.xui.PlayerInventory.AddItem(itemStack))
-					uiForPlayer.xui.PlayerInventory.DropItem(itemStack);
-
-				world.SetBlockRPC(_clrIdx, vector3i, BlockValue.Air);
-			}
+			GameManager.ShowTooltip(entityPlayerLocal, Localization.Get("ttCantPickupInUse"), string.Empty, "ui_denied");
+			return;
 		}
+		LocalPlayerUI uIForPlayer = LocalPlayerUI.GetUIForPlayer(entityPlayerLocal);
+		HandleTakeInternalItems(tileEntity, uIForPlayer);
+		ItemStack itemStack = new ItemStack(block.ToItemValue(), 1);
+		if (!uIForPlayer.xui.PlayerInventory.AddItem(itemStack))
+		{
+			uIForPlayer.xui.PlayerInventory.DropItem(itemStack);
+		}
+		world.SetBlockRPC(clrIdx, vector3i, BlockValue.Air);
 	}
 
-    // copied from ocbClaimAutoRepair
+    // copied from BlockWorkStation
 	protected virtual void HandleTakeInternalItems(TileEntityEfficientBaseRepair te, LocalPlayerUI playerUI)
 	{
 		ItemStack[] items = te.items;
-		for (int index = 0; index < items.Length; ++index)
+		for (int i = 0; i < items.Length; i++)
 		{
-			if (!items[index].IsEmpty() && !playerUI.xui.PlayerInventory.AddItem(items[index]))
-				playerUI.xui.PlayerInventory.DropItem(items[index]);
+			if (!items[i].IsEmpty() && !playerUI.xui.PlayerInventory.AddItem(items[i]))
+			{
+				playerUI.xui.PlayerInventory.DropItem(items[i]);
+			}
 		}
 	}
+
 }
